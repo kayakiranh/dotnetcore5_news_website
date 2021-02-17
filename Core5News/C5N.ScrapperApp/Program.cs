7cf82174b5c0a3e4d16be8cc266542bf54299e91
kayakiranh <kayakiranh@hotmail.com>
2021-02-17 21:20:13 +0300

    katmanlar oluşturuldu.

---
+++ Core5News/C5N.ScrapperApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C5N.ScrapperApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
